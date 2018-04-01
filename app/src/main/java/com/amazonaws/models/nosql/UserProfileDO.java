38dda164cd3bb63e27ab09f9036edc8bc5ff99f0
Vivek Sharma <vivekmsit@gmail.com>
2018-04-01 18:43:52 +0530

    Added dynamodb generated java files.

---
+++ app/src/main/java/com/amazonaws/models/nosql/UserProfileDO.java
@@ -0,0 +1,38 @@
+package com.amazonaws.models.nosql;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@DynamoDBTable(tableName = "shybird-mobilehub-1428559324-UserProfile")
+
+public class UserProfileDO {
+    private String _userId;
+    private String _emailId;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    @DynamoDBAttribute(attributeName = "userId")
+    public String getUserId() {
+        return _userId;
+    }
+
+    public void setUserId(final String _userId) {
+        this._userId = _userId;
+    }
+    @DynamoDBAttribute(attributeName = "emailId")
+    public String getEmailId() {
+        return _emailId;
+    }
+
+    public void setEmailId(final String _emailId) {
+        this._emailId = _emailId;
+    }
+
+}
