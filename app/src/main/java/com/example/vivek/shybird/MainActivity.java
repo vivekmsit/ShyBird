87665056a62f5c10e5c364a454bb59eeb7ee2d77
Vivek Sharma <vivekmsit@gmail.com>
2018-04-02 00:02:28 +0530

    Added logic to create table in dynamodb. not tested yet.

--- app/src/main/java/com/example/vivek/shybird/MainActivity.java
+++ app/src/main/java/com/example/vivek/shybird/MainActivity.java
@@ -1,16 +1,69 @@
 package com.example.vivek.shybird;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    // Declare a DynamoDBMapper object
+    AmazonDynamoDBClient dynamoDBClient;
+    DynamoDBMapper dynamoDBMapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AWSMobileClient.getInstance().initialize(this).execute();
+
+        // Instantiate a AmazonDynamoDBMapperClient
+        dynamoDBClient = new AmazonDynamoDBClient(AWSMobileClient.getInstance().getCredentialsProvider());
+        dynamoDBMapper = DynamoDBMapper.builder()
+                .dynamoDBClient(dynamoDBClient)
+                .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
+                .build();
+        createTestTable();
+    }
+
+    void createTestTable() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                createTable();
+            }
+        }).start();
+    }
+
+    void createTable() {
+        KeySchemaElement kse = new KeySchemaElement().withAttributeName(
+                "userNo").withKeyType(KeyType.HASH);
+        AttributeDefinition ad = new AttributeDefinition().withAttributeName(
+                "userNo").withAttributeType(ScalarAttributeType.N);
+        ProvisionedThroughput pt = new ProvisionedThroughput()
+                .withReadCapacityUnits(10L).withWriteCapacityUnits(5L);
+
+        CreateTableRequest request = new CreateTableRequest()
+                .withTableName("TestTable")
+                .withKeySchema(kse).withAttributeDefinitions(ad)
+                .withProvisionedThroughput(pt);
+        try {
+            Log.d(TAG, "Sending Create table request");
+            dynamoDBClient.createTable(request);
+            Log.d(TAG, "Create request response successfully recieved");
+        } catch (AmazonServiceException ex) {
+            Log.e(TAG, "Error sending create table request", ex);
+        }
     }
 }
